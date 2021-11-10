f82b3e37eadd9777cfa3ec8d3b3bb54c99951e79
Pawel Gasiorek <pawel.gasiorek@beekeeper.io>
2021-11-10 15:46:30 +0100

    aaa

--- src/main/java/basic/TestClass.java
+++ src/main/java/basic/TestClass.java
@@ -3,4 +3,9 @@ package basic;
 public class TestClass {
     private static String xx = "aaa";
     private static String xx1 = "aaa";
+    private static String xx11 = "aaa";
+    private static String xx12 = "aaa";
+    private static String xx122 = "aaa";
+    private static String xx133 = "aaa";
+    private static String xx144 = "aaa";
 }
