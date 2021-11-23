f1263a54c530c65333b40b9e2f4239746ce0b6b3
Pawel Gasiorek <pawel.gasiorek@beekeeper.io>
2021-11-23 13:24:02 +0100

    aaa

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
+;
+gradlew.bat,2/a/2a45a911a8f1836b0b6c5b758962572012d8f8c3
+R
+"src/main/java/basic/TestClass.java,a/1/a160bd1e6cd37b4030f4de6077363b83422ad9f4
--- src/main/java/basic/TestClass.java
+++ src/main/java/basic/TestClass.java
@@ -1,11 +1,28 @@
 package basic;
 
+import java.util.List;
+
 public class TestClass {
-    private static String xx = "aaa";
-    private static String xx1 = "aaa";
-    private static String xx11 = "aaa";
-    private static String xx12 = "aaa";
-    private static String xx122 = "aaa";
-    private static String xx133 = "aaa";
-    private static String xx144 = "aaa";
+    public static List simpleList = List.of("aa", "bb", "ccc");
+
+    public static void main(String[] args) {
+        String textExample = "aaaa";
+
+        int unusedVariable = 111;
+        methodOne("aa");
+        methodTwo("aa");
+        System.out.println(textExample);
+    }
+
+    static void methodOne(String value) {
+        System.out.println(value);
+    }
+
+    static void methodTwo(String text) {
+        System.out.println(text);
+    }
+
+//    boolean getNumberOfElementsInsideList(List list, String element) {
+//        return simpleList.stream().filter($ -> $.equals(element));
+//    }
 }
